4100074f0f6f807c7316266b4e575615da9eabf6
Yevheniia Mazur <yevheniia.mazur@valor-software.com>
2017-12-07 18:04:14 +0200

    feat(tests): add bdd run for multiple instance at travis

--- protractor.cucumber.js
+++ protractor.cucumber.js
@@ -1,9 +1,3 @@
-let envUrl = "http://ngx-bootstrap-latest.surge.sh/#/";
-
-if (process.env.TRAVIS) {
-  envUrl = "http://localhost:3000/"
-}
-
 const chromeOptions = {
   // need this window size due to cutting last menu point by viewport borders. will be deleted after fix
   args: ['--headless', '--disable-gpu', '--window-size=800, 900']
@@ -17,7 +11,7 @@ exports.config = {
 
   SELENIUM_PROMISE_MANAGER: false,
 
-  baseUrl: envUrl,
+  baseUrl: process.env.URL,
 
   capabilities: {
     'browserName': 'chrome',
